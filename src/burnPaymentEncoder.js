cbd6320bb26012621304351b458500861a931f20
Ji Sheng <jsheng0811@gmail.com>
2020-04-10 02:34:06 +0800

    Added Burn Payment Encoder Test

--- src/burnPaymentEncoder.js
+++ src/burnPaymentEncoder.js
@@ -44,7 +44,7 @@ module.exports = {
   },
 
   encodeBulk: function (paymentsArray, isBurn) {
-    var payments = Buffer.from(0)
+    var payments = Buffer.alloc(0)
     var amountOfPayments = paymentsArray.length
     for (var i = 0; i < amountOfPayments; i++) {
       var payment = paymentsArray[i]
